2febd3409eff47cbaff06345fa34bc2544c1a8b2
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-10 19:46:41 +0530

    Add a post route to /api/driver

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,6 @@ const driver_controller = require('../controllers/driver_controller');
 
 module.exports = (app) =>{
 	app.get('/api', driver_controller.greeting);
+
+	app.post('/api/driver', driver_controller.create);
 }
