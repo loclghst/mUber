f7d893ecaec2e5c3b207f09ed73b0cdd84c2d74a
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-10 19:46:06 +0530

    Define a controller for driver model

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const app = express();
 const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+const app = express();
 
 
+//Put app.use(bodyParser.json()) before routes(app)
+
+// app.use(bodyParser.json()) means consider all input as json and convert it to Js object
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -1,5 +1,10 @@
 module.exports = {
 	greeting(req,res){
 		res.send({Hey: 'there'}); 
+	},
+
+	create(req,res){
+		console.log(req.body);
+		res.send({Hey: 'there'}); 
 	}
 }
