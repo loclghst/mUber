46e23528a2aad52f0e8bacdf6d5e49350f8106ad
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-10 21:05:17 +0530

    Set a different db for development testing

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
+//set mongoose to use ES6 promise
+mongoose.Promise = global.Promise;
+
+//connect to muber db if server is not run in test mode
+if(process.env.NODE_ENV !== 'test')
+	mongoose.connect('mongodb://localhost/muber',{useNewUrlParser: true});
 
 //Put app.use(bodyParser.json()) before routes(app)
 
