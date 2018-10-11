0038e0b3e717b4fad57fc6e37ec63cd01f77bb89
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-11 16:40:30 +0530

    Add a geoJSON object as a sub document to the Driver schema

--- model/driver.js
+++ model/driver.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//creating a  schema for geoJSON type point
+//We will embedd it as a sub document
+
+const PointSchema = new Schema({
+	type:{ type: String, default:'Point'},
+	coordinates : {type: [Number], index: '2dsphere'}
+	//the index property here is what tells mongoDB that its
+	//a very special coordinate system or a very special 
+	//property right here of which mongoDB should keep track of
+	//and consider to be an index for geoJSON type queries
+
+});
+
+//we have defined the PointSchema, now we can embedd it as a 
+//subdocument inside the DriverSchema
+
 const DriverSchema = new Schema({
 	email: {
 		type: String,
@@ -9,7 +25,9 @@ const DriverSchema = new Schema({
 	driving:{
 		type: Boolean,
 		default: false
-	}
+	},
+	//embedding PointSchema as a subdocument
+	geometry: PointSchema
 });
 
 
