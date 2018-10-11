3aaa104f5efbce38fd66dc6e4b57e7ff18944842
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-11 16:41:57 +0530

    Add the route handler for the geoNear query

--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -5,6 +5,39 @@ module.exports = {
 		res.send({Hey: 'there'}); 
 	},
 
+	index(req, res, next){
+
+		//the centre point of the query should be the current position
+		//of the user, so we will expect the req- onject to contain
+		//the current location of the user i.e the req object will
+		//supply the lng and lat paramentes to coordinates property
+
+		//But this will be a GET request, and get request do not contain 
+		//the req body. so rather than looking at req.body, we 
+		//will get the lng and lat from the query string as follows
+
+		const {lng, lat} = req.query;
+
+
+
+		//to get nearby drivers we run a geoNear() query
+		//geoNear() query takes a geoJSON object as first paramete
+		//A geoJSON object is a object which has type and coordinates property
+		//Second parameter is Options
+		Driver.geoNear(
+			//first parameter- geoJSON object
+			{type:'Point',coordinates:[lng,lat]},
+			//second parameter - Options
+			//spherical: true means we are telling mongo that the query
+			//is made on a spherical object
+			//maxDistance specifies the maximum distance over which to return results
+			{spherical: true,maxDistance: 200000}
+		)
+		//this returns a promise so we will cahin a .then()
+		.then((drivers) => res.send(drivers))
+		.catch(done);
+	},
+
 	create(req,res,next){
 		const driverProps = req.body;
 		Driver.create(driverProps)
