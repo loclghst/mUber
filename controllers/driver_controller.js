550305b6299b277ec156f50f6e779fb0a91fe984
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-11 14:34:10 +0530

    Add route handler to edit a particualr driver

--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -10,5 +10,18 @@ module.exports = {
 		Driver.create(driverProps)
 			  .then(driver => res.send(driver))
 			  .catch(next); 
+	},
+
+	edit(req,res,next){
+		const driverId = req.params.id;
+		const driverProps = req.body;
+
+		Driver.findByIdAndUpdate({_id: driverId}, driverProps)
+		//findByIdAndUpdate behaves a bit differently than other mongo operations. We'd expect it to return 
+		//the driver that is just updates but instead it returns some statistics about the updated driver, not 
+		//the driver itself
+			  .then(() => Driver.findById({_id : driverId}))
+			  .then((driver) => res.send(driver))
+			  .catch(next); 
 	}
 }
